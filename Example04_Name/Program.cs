6deefeedd45d1a3c5103e8536ee5b0538c825b1b
LegendaVselennoy <rom.ahmedhanov@yandex.ru>
2022-08-29 18:05:25 +0300

    Example 04 Name

---
+++ Example04_Name/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What's your name user: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, this Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.Write(username);
+}
